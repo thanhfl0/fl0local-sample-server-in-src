126f5d41a3a637a8375d49bc204ba03377ac11c8
thanhfl0 <104146761+thanhfl0@users.noreply.github.com>
2022-10-24 23:36:58 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 const http = require("http");
-const server = http.createServer((req, res) => {});
+
+const server = http.createServer(async (req, res) => {
+  //set the request route
+  if (req.url === "/healthcheck") {
+    //response headers
+    res.writeHead(200, { "Content-Type": "application/json" });
+    //set the response
+    res.write("Hi there, This is a Vanilla Node.js API");
+    //end the response
+    res.end();
+  }
+
+  // If no route present
+  else {
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ message: "Route not found" }));
+  }
+});
 
 server.listen(3000, () => {
   console.log(`Server is running`);
